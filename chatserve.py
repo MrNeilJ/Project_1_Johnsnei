434096f4408af1aafbfc8c214ae1fe1766cdbbe5
MrNeilJ <mr.neil.j@gmail.com>
2018-10-28 09:48:20 -0700

    Initial commit

---
+++ chatserve.py
@@ -0,0 +1,9 @@
+#!/bin/python
+
+#################################################
+#   Name: Neil Johnson                          #
+#   Due: 10/28/18                               #
+#   Assignment: Project 1                       #
+#   Details: Create a simple chat client        #
+#################################################
+
