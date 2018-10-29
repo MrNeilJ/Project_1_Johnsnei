84db08e2c9fc11cf9e6db20c04b80cfd6934fc72
MrNeilJ <mr.neil.j@gmail.com>
2018-10-28 18:27:37 -0700

    Fixing issues where the application would not run.

--- chatserve.py
+++ chatserve.py
@@ -69,7 +69,7 @@ def name_exchange(connection_socket, user):
 
 
 # Create the continuous chatting system between server and client.
-def chat_feature(connectionSocket, msg_size, client, user):
+def chat_feature(connectionSocket, client, user):
     # Clear out terminal to make it look fresh
     clear_terminal()
 
@@ -83,7 +83,7 @@ def chat_feature(connectionSocket, msg_size, client, user):
 
     while True:
         # Get the message from the client up to a specific amount specified earlier.
-        client_msg = connectionSocket.recv(msg_size)[0:-1]
+        client_msg = connectionSocket.recv(500)[0:-1]
 
         if client_msg == "":
             print("Terminating connection.")
@@ -125,7 +125,7 @@ if __name__ == "__main__":
         connectionSocket, addr = serverSocket.accept()
 
         # Create the chat feature between the user and the server
-        chat_feature(connectionSocket, 501, name_exchange(connectionSocket, username), username)
+        chat_feature(connectionSocket, name_exchange(connectionSocket, username), username)
 
         # Close the connection once done with the client
         connectionSocket.close()
