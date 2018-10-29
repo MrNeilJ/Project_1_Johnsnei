62e234bffef99cfce937470297a0db3cbf9a6440
MrNeilJ <mr.neil.j@gmail.com>
2018-10-28 18:18:09 -0700

    Pushing most recent additions for further triage on server.

--- chatclient.c
+++ chatclient.c
@@ -41,7 +41,7 @@ struct addrinfo* setAddressInfo(char* address, char* port){
 }
 
 
-int makeSocket(struct addrInfo* res) {
+int makeSocket(struct addrinfo* res) {
     int sockfd;
 
     if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
